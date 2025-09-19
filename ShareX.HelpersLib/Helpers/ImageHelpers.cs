212e456944aad35920fd3a7e440784cf343f7d3c
cryptofyre <cryptofyre@cryptofyre.org>
2025-09-19 15:14:29 -0500

    Add AVIF IQ tuning options
    - Introduced a new enum `AvifTuneIQ` to specify tuning options for AVIF encoding.
    - Updated `SaveImageAsStream` and related methods to accept and utilize the `AvifTuneIQ` parameter.
    - Enhanced the `TaskSettings` to include AVIF tuning settings in the UI.
    - Modified the `TaskSettingsForm` to allow users to select AVIF tuning options, improving image quality control during AVIF encoding.

--- ShareX.HelpersLib/Helpers/ImageHelpers.cs
+++ ShareX.HelpersLib/Helpers/ImageHelpers.cs
@@ -2319,7 +2319,7 @@ namespace ShareX.HelpersLib
                 }
             }
         }
-        public static bool SaveAvif(Image img, string filePath, int quality = 80, int speed = 6)
+        public static bool SaveAvif(Image img, string filePath, int quality = 80, int speed = 6, AvifTuneIQ tuneIQ = AvifTuneIQ.Default)
         {
             if (img == null)
                 throw new ArgumentNullException(nameof(img));
@@ -2330,7 +2330,7 @@ namespace ShareX.HelpersLib
             {
                 using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
                 {
-                    return SaveAvifToStream(img, fileStream, quality, speed);
+                    return SaveAvifToStream(img, fileStream, quality, speed, tuneIQ);
                 }
             }
             catch (Exception ex)
@@ -2340,7 +2340,7 @@ namespace ShareX.HelpersLib
             }
         }
 
-        public static bool SaveAvifToStream(Image img, Stream outputStream, int quality = 80, int speed = 6)
+        public static bool SaveAvifToStream(Image img, Stream outputStream, int quality = 80, int speed = 6, AvifTuneIQ tuneIQ = AvifTuneIQ.Default)
         {
             if (img == null)
                 throw new ArgumentNullException(nameof(img));
@@ -2379,7 +2379,7 @@ namespace ShareX.HelpersLib
                     }
 
                     // Create and configure encoder
-                    encoder = CreateAndConfigureEncoder(quality, speed);
+                    encoder = CreateAndConfigureEncoder(quality, speed, tuneIQ);
                     if (encoder == IntPtr.Zero)
                         return false;
 
@@ -2466,7 +2466,7 @@ namespace ShareX.HelpersLib
             return true;
         }
 
-        private static IntPtr CreateAndConfigureEncoder(int quality, int speed)
+        private static IntPtr CreateAndConfigureEncoder(int quality, int speed, AvifTuneIQ tuneIQ)
         {
             var encoder = NativeMethods.avifEncoderCreate();
             if (encoder == IntPtr.Zero)
@@ -2489,6 +2489,22 @@ namespace ShareX.HelpersLib
 
                 // Write settings back to the encoder
                 Marshal.StructureToPtr(settings, encoder, false);
+                // Apply codec-specific tune option if requested, after writing settings
+                try
+                {
+                    if (tuneIQ == AvifTuneIQ.PSNR)
+                    {
+                        NativeMethods.avifEncoderSetCodecSpecificOption(encoder, "tune", "psnr");
+                    }
+                    else if (tuneIQ == AvifTuneIQ.SSIM)
+                    {
+                        NativeMethods.avifEncoderSetCodecSpecificOption(encoder, "tune", "ssim");
+                    }
+                }
+                catch (Exception ex2)
+                {
+                    DebugHelper.WriteException(ex2, "Failed to set AVIF tune option");
+                }
                 return encoder;
             }
             catch (Exception ex)
